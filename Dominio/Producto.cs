8e63d4b2e72be675b36b2742ee3822bc0da03c82
JesusEs1312 <estrada.jesus1312@gmail.com>
2021-12-06 20:30:45 -0600

    se agrego el dominio producto

---
+++ Dominio/Producto.cs
@@ -0,0 +1,11 @@
+namespace Dominio
+{
+    public class Producto
+    {
+        public int id {get; set;}
+        public string marca {get; set;}
+        public string fabricante {get; set;}
+        public decimal precio {get; set;}
+        public string codigo_barra {get; set;} 
+    }
+}
