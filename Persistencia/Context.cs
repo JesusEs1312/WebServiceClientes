4e3dea34a729910909e358815d0ece5560255310
JesusEs1312 <estrada.jesus1312@gmail.com>
2021-12-21 11:27:10 -0600

    Se modifico la herencia de la Clase Context a IdentityDbContext y se crearon las tabla de migracion

--- Persistencia/Context.cs
+++ Persistencia/Context.cs
@@ -1,13 +1,19 @@
 
 using Dominio;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistencia
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<Usuario>
     {
         public Context(DbContextOptions options) : base(options)
         {    
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
         
         public DbSet<Producto> Producto {get; set;}//Entidad para la tabla productos de SQL Server
