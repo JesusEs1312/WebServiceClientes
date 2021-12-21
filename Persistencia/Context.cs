e112da9821544b1b52161cf1adec522c67cef58a
JesusEs1312 <estrada.jesus1312@gmail.com>
2021-12-21 11:46:09 -0600

    Se realizo el procedimiento de migracion en la clase Program

--- Persistencia/Context.cs
+++ Persistencia/Context.cs
@@ -13,9 +13,15 @@ namespace Persistencia
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Crear las tablas de migracion a SQL Server
             base.OnModelCreating(modelBuilder);
         }
         
         public DbSet<Producto> Producto {get; set;}//Entidad para la tabla productos de SQL Server
     }   
 }
+
+//NOTA: Comando para instalar dotnet-ef
+//dotnet tool install --global dotnet-ef --version 5.0.0
+//NOTA: Comando para crear la carpeta con las tablas de migracion
+//dotnet ef migrations add IdentityCoreInicial -p Persistencia/ -s WebAPI
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistencia;
 
 namespace WebAPI
 {
@@ -13,7 +16,25 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //Procedimiento para ejecutar la migracion de tablas a SQL Server
+            var hostServer = CreateHostBuilder(args).Build();
+            //Creamos un ambiente (Scope)
+            using(var ambiente = hostServer.Services.CreateScope())
+            {
+                var services = ambiente.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<Context>();
+                    //Realizamos la Migracion s SQL Server
+                    context.Database.Migrate();
+                } catch (Exception e)
+                {
+                    var loggin = services.GetRequiredService<ILogger<Program>>();
+                    loggin.LogError(e, "Ha ocurrido un error en la migracion");
+                }
+            }
+            //Ejecutamos el server
+            hostServer.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
